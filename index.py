c0083c9685d8b9ee661365acf9e1f98ea067de6c
GauravC <email@gmail.com>
2022-02-25 11:35:17 +0530

    tweaks to the correction system for the questions

--- index.py
+++ index.py
@@ -1,7 +1,4 @@
-from os import sep
-from tkinter.messagebox import QUESTION
 import xlrd 
-import random 
 import pandas
 
 work_book = pandas.read_excel(r"C:\Users\user\Desktop\Question-Paper-Generator\Questions_database1.xlsx")
@@ -9,6 +6,8 @@ work_book = pandas.read_excel(r"C:\Users\user\Desktop\Question-Paper-Generator\Q
 df = pandas.DataFrame(work_book)
 for i in range(1,10):
     var=df.sample(n=1)
+    var_index=var.index.tolist()
+    print(var_index[0]+2)
     print('\n')
     print(i,".",sep='',end=' ')
     print('\n'.join(var.to_string(index=False).split('\n')[1:]))
