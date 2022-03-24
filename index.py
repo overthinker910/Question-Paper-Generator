912e3e8eba8d37aefcf67f56cb303c2400a373c7
GauravC <email@gmail.com>
2022-03-24 14:35:07 +0530

    chnages to app.py

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ import pandas
 work_book = pandas.read_excel(r"C:\Users\user\Desktop\Question-Paper-Generator\Questions_database1.xlsx")
 var_index1=[]
 df = pandas.DataFrame(work_book)
+seq=df[["Question"]]
 for i in range(1,10):
     var=df.sample(n=1)
     var_index=var.index.tolist()
@@ -12,7 +13,7 @@ for i in range(1,10):
     print(var_index[0]+2)
     print('\n')
     print(i,".",sep='',end=' ')
-    print('\n'.join(var.to_string(index=False).split('\n')[1:]))
+    print(var.to_string(index=False).split('\n')[1].split("?")[0])
     
 for i in range(len(var_index)):
     var_index1.append(var_index[i])
---
+++ try.py
@@ -0,0 +1,3 @@
+x = "what i su r name? yash"
+y = x.split("?")
+print(y[0])
