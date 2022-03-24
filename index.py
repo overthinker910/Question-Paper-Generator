56ee465cc20b77724ae00ee394a26f11178f93fa
GauravC <email@gmail.com>
2022-03-24 23:42:05 +0530

    chnages to index.py

--- index.py
+++ index.py
@@ -4,22 +4,32 @@ import pandas
 
 work_book = pandas.read_excel(r"C:\Users\user\Desktop\Question-Paper-Generator\Questions_database1.xlsx")
 var_index1=[]
+score=0
 df = pandas.DataFrame(work_book)
-seq=df[["Question"]]
-for i in range(1,10):
+j=0
+for i in range(1,5):
     var=df.sample(n=1)
     var_index=var.index.tolist()
     var_index1.append(var_index[0]+2)
-    print(var_index[0]+2)
     print('\n')
     print(i,".",sep='',end=' ')
-    print(var.to_string(index=False).split('\n')[1].split("?")[0])
-    
+    print(var.to_string(index=False).split('\n')[1].split("?")[0]+"?")
+    str=input("Enter the answer: ")
+    if str==df.iloc[var_index1[j]-2,1].lower():
+        score+=2
+    j=j+1
+
 for i in range(len(var_index)):
     var_index1.append(var_index[i])
 
 var_index1.remove(var_index1[-1])
-print(var_index1)
+    
+print("\n\nThe score of your test is: ")
+print(score)
 
-print(df.iloc[var_index1[0],1])
-#code prints 10 questions in a random order
+print("\nFind below the answers of this test: ")
+for i in range(0,4):
+    print(i+1,".",sep='',end=' ')
+    print(df.iloc[var_index1[i]-2,1])
+
+print()
